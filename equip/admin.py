6b3d60fe4386bfcbe066ac84e7569cede3d52cc6
Chih-Wei Fang <dodo0822@hs.ntnu.edu.tw>
2016-06-24 23:45:05 +0800

    add location field to table admin

--- equip/admin.py
+++ equip/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Location)
 admin.site.register(Event)
 
 class EquipmentTableAdmin(admin.ModelAdmin):
-    list_display = ('manufacturer', 'model', 'number', 'condition', 'belongs_to')
-    list_editable = ('number', 'condition', 'belongs_to')
+    list_display = ('manufacturer', 'model', 'number', 'condition', 'belongs_to', 'location')
+    list_editable = ('number', 'condition', 'belongs_to', 'location')
 
 admin.site.register(Equipment, EquipmentTableAdmin)
