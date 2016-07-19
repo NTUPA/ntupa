a850bc9dedd15f041f4ca31634dfb08b59c19aa2
Sean Liao <seanliao96@gmail.com>
2016-07-19 13:14:06 +0800

    add favicon and home link

--- ntupa/urls.py
+++ ntupa/urls.py
@@ -20,6 +20,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^login/$', views.login, name='login'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
